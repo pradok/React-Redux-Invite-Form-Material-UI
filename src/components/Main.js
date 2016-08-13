14ef0ec0b371c36c57be035d48b1fb0639e22a20
Pradosh Sinha <pradoshks@outlook.com>
2016-08-14 01:35:26 +1000

    Added app layout structure with basic sass

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,26 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <header>
+                    Header
+                </header>
+                <main>
+                    <p>Form here</p>
+                </main>
+                <footer>
+                    Footer
+                </footer>
+            </div>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
