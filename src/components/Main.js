b1d4d72dc80bca8da1b0ad9fde87378ad1f521d1
Pradosh Sinha <pradoshks@outlook.com>
2016-08-15 22:04:00 +1000

    Added css lib

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,26 @@
 require('normalize.css/normalize.css');
 require('styles/App.scss');
+require("muicss/lib/sass/mui.scss");
 
 import React from 'react';
+import Button from 'muicss/lib/react/button';
 
 class AppComponent extends React.Component {
     render() {
         return (
             <div className="app">
                 <header>
-                    Header
+                    Broccoli & CO
                 </header>
                 <main>
-                    <p>Form here</p>
+                    <h2>A better way to enjoy every day</h2>
+                    <p>Be the first to know when we launch</p>
+                    <Button color="primary">Request an Invite</Button>
                 </main>
                 <footer>
-                    Footer
+                    <em>Made with love in Melbourne<br/>
+                        &copy; {new Date().getFullYear()} Broccoli & Co. All right reserved
+                    </em>
                 </footer>
             </div>
         );
